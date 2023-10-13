06ccfd044fccb64b856343fddbfb853593dbabc5
Joana Pereira <130710863+jcspereira76@users.noreply.github.com>
2023-10-13 12:42:22 +0100

    Add files via upload

--- include/ModbusAP.h
+++ include/ModbusAP.h
@@ -13,11 +13,11 @@
 #define MIN_START_ADDR 0x0000 //Min starting address
 #define MAX_START_ADDR 0xFFFF //Max starting address
 
-#define FC_READ 0x03 //3
-#define FC_WRITE 0x10 //16
+#define FC_READ 0x03 //Function Code - 3
+#define FC_WRITE 0x10 //Function Code - 16
 
-int read_h_regs( char *server_add, __uint16_t start_addr, __uint16_t n_reg );
+int read_h_regs( int sockfd, __uint16_t start_addr, __uint16_t n_reg, __uint16_t *Read_reg);
 
-int write_multiple_regs( char *server_add, __uint16_t start_addr, __uint16_t n_r, __uint16_t *val );
+int write_multiple_regs( int sockfd, __uint16_t start_addr, __uint16_t n_reg, __uint16_t *val );
 
 #endif
--- include/ModbusTCP.h
+++ include/ModbusTCP.h
@@ -6,14 +6,23 @@
 #include <netinet/in.h>
 #include <sys/types.h>
 #include <arpa/inet.h>
+#include <sys/time.h>
+#include <netdb.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <fcntl.h>
 #include <stdlib.h>
+#include <signal.h>
 
 
-#define PORT 502
+
+#define UNIT_ID 51
 #define MBAP_LEN 7
 
-int send_modbus_request( char *server_add, __uint8_t *APDU, __uint16_t APDUlen, __uint8_t *APDU_R );
+
+int connect_socket( char *server_add, int port );
+int close_socket( int sockfd );
+
+int send_modbus_request( int sockfd, __uint8_t *APDU, __uint16_t *APDUlen, __uint8_t *APDU_R );
 
 #endif
